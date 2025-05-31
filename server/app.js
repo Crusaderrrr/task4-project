140179c63a5cea88a7e27ff3f7aa70e7fce0b672
Nikita Bulash <bulash2006@gmail.com>
2025-05-31 11:25:44 +0100

    Added backend cors for netlify

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const users = require('./routes/users');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:5173', credentials: true })); 
+app.use(cors({ origin: 'https://playful-conkies-934942.netlify.app', credentials: true })); 
 
 app.use('/api/auth', auth);
 app.use('/api/users',checkUserStatus, users);
